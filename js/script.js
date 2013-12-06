e481d4b9129444052aa914767ec5413323b863cc
PeterTheOne <petertheone@gmail.com>
2013-12-06 16:23:02 +0100

    add scroll to result and submit button color change.

--- js/script.js
+++ js/script.js
@@ -2,9 +2,18 @@ var result = $('#result');
 
 $(function() {
 
+    $('#candidates').change(updateButton).keydown(updateButton);
+    $('#ballots').change(updateButton).keydown(updateButton);
+    $('#incompleteBallots').change(updateButton);
+
+    function updateButton() {
+        $('#submit').html('Submit').removeClass('btn-success').addClass('btn-primary');
+    }
+
     $('#submit').click(function(event) {
         event.preventDefault();
         result.html('');
+        $('#submit').html('Re-Submit').removeClass('btn-primary').addClass('btn-success');
         $('#result-group').show();
 
         var incompleteBallots = $('#incompleteBallots').is(':checked');
@@ -14,6 +23,8 @@ $(function() {
         if (Irv.validateInput(candidateNames, ballots, incompleteBallots)) {
             Irv.calculateWinner(candidateNames, ballots);
         }
+
+        $('html, body').animate({scrollTop: $(document).height()}, 'slow');
     });
 
 });
