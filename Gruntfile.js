01929df9508c40f541dff09b26a407c0b934a4bb
PeterTheOne <petertheone@gmail.com>
2014-04-07 14:31:33 +0200

    rename readme, add converalls stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,10 +3,18 @@ module.exports = function(grunt) {
     grunt.initConfig({
         qunit: {
             files: ['test/index.html']
+        },
+        coveralls: {
+            options: {
+                // dont fail if coveralls fails
+                force: true
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-coveralls');
+    grunt.loadNpmTasks('grunt-qunit-istanbul');
 
     // Task to run tests
     grunt.registerTask('test', 'qunit');
